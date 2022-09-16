5403eafe4e435f19b489a102df568cab73147440
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-16 20:20:03 +0300

    adding flip

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+
+const card1 = document.getElementById("1");
+const card2 = document.getElementById("2");
+const card3 = document.getElementById("3");
+const card4 = document.getElementById("4");
+const card5 = document.getElementById("5");
+const card6 = document.getElementById("6");
+const cards = document.querySelectorAll('.card');
+
+
+[...cards].forEach((card)=>{
+  card.addEventListener( 'click', function() {
+    card.classList.toggle('is-flipped');
+  });
+});
+
