bfdef1510021e47df9171426adc242b0e841ebb1
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-16 21:27:57 +0300

    loop to fill the cards

--- js/javascript.js
+++ js/javascript.js
@@ -6,6 +6,7 @@ const card4 = document.getElementById("4");
 const card5 = document.getElementById("5");
 const card6 = document.getElementById("6");
 const cards = document.querySelectorAll('.card');
+const card_back = document.querySelectorAll('.card_back > img');
 
 
 [...cards].forEach((card)=>{
@@ -23,3 +24,21 @@ const randomNum = (max, used) => {
     }
     return newNum;
 }
+
+// fill cards
+let usedNums = [];
+for(let i=0 ; i < 6 ;i++){
+
+    let randNum = randomNum(6, usedNums);
+    usedNums.push(randNum);
+    if (i < 2){
+        card_back[randNum-1].src = "./images/html_logo.png";
+    }
+    else if (i < 4){
+        card_back[randNum-1].src = "./images/css_logo.png";
+    }
+    else {
+        card_back[randNum-1].src = "./images/js_logo.png";
+    }
+    
+ }
